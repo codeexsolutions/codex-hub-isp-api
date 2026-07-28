dbdbb171a13d0f3ade823dd8b665d150a204042a
codexsolutions-tech <codexsolutions9@gmail.com>
2026-07-28 15:11:53 -0300

    feat: retira autenticação da tora de chave publlica

--- src/api/routes/notifications.route.ts
+++ src/api/routes/notifications.route.ts
@@ -7,7 +7,7 @@ const notificationRoute = Router();
 const controller = container.resolve(NotificationsController);
 
 notificationRoute.post('/salvar', authMiddleware, controller.Salvar.bind(controller));
-notificationRoute.get('/public-key', authMiddleware, controller.ObterPublicKey.bind(controller));
+notificationRoute.get('/public-key', controller.ObterPublicKey.bind(controller));
 notificationRoute.post('/notificar', authMiddleware, controller.EnviarNotificacao.bind(controller));
 notificationRoute.get('/buscarTodos', authMiddleware, controller.OberTodos.bind(controller));
 notificationRoute.get('/buscarPorCpf', authMiddleware, controller.EnviarNotificacao.bind(controller));
